db857efb97abc38a15ddedbd3248f3cc540d3277
Nathan Harris <nharrisanalyst@gmail.com>
2025-04-14 16:03:54 -0700

    annotations

--- nba_over_time/src/chart.js
+++ nba_over_time/src/chart.js
@@ -1,5 +1,10 @@
 import * as d3 from 'd3';
-import { selectAll } from 'd3';
+import { annotationLabel, annotation, annotationCalloutCircle } from 'd3-svg-annotation';
+import { variance } from 'simple-statistics';
+
+
+
+const avg_line_color = 'grey';
 
 export async function createChart(element_1, element_2){
    const nba_data = await d3.csv('/data/nba_advanced_year.csv',d=>({
@@ -25,33 +30,37 @@ export async function createChart(element_1, element_2){
       min: d3.min(d_year, d=>d.off_rating),
       max: d3.max(d_year, d=>d.off_rating),
       avg: d3.mean(d_year, d=>d.off_rating),
+      variance:variance(d_year.map(d=>d.off_rating)),
       year_date:d_year[0].year_date
- }))
+ })).filter(d=>d.year !='2024-25')
   console.log(agg_stats_off)
  const agg_stats_def = years_groupby_year.map(d_year=>({
     year:d_year[0].year,
     min: d3.min(d_year, d=>d.def_rating),
     max: d3.max(d_year, d=>d.def_rating),
     avg: d3.mean(d_year, d=>d.def_rating),
+    variance:variance(d_year.map(d=>d.def_rating)),
     year_date:d_year[0].year_date
-}))
+})).filter(d=>d.year !='2024-25')
 
  console.log(agg_stats_off, agg_stats_def);
 
 //chart 
 
-const margin ={t:20,r:20,b:65,l:45};
-const height = 545 - (margin.r + margin.l);
-const width = 928- (margin.t + margin.b);
-const svg_offense = d3.select(element_1).append('svg').attr('height', height + (margin.r + margin.l)).attr('width', width + (margin.l + margin.r));
+const margin ={t:60,r:70,b:200,l:45};
+const height = 740 - (margin.b + margin.t);
+const width = 958- (margin.l + margin.r);
+const svg_offense = d3.select(element_1).append('svg').attr('height', height + (margin.b + margin.t)).attr('width', width + (margin.l + margin.r));
 
 const mainG_off = svg_offense.append('g').attr('transform', `translate(${margin.l},${margin.t})`)
 //scales
 const xScale= d3.scaleTime(d3.extent(agg_stats_off, d=> d.year_date),[0,width])
 const yScale= d3.scaleLinear([d3.min(agg_stats_off, d=>d.min), d3.max(agg_stats_def, d=>d.max)], [height, 0]).nice()
 
+const yScaleDeff = d3.scaleLinear([d3.min(agg_stats_def, d=>d.min), d3.max(agg_stats_def, d=>d.max)], [0,height]).nice();
+
 //axis 1
-mainG_off.append('g').attr('class', 'y-axis').attr("transform", `translate(${margin.l}},0)`)
+mainG_off.append('g').attr('class', 'y-axis').attr("transform", `translate(0,0)`)
                                             .call(d3.axisLeft(yScale))
                                             .call(g => g.select(".domain").remove())
                                             .call(g => g.selectAll(".tick line").clone()
@@ -62,16 +71,30 @@ mainG_off.append('g').attr('class', 'y-axis').attr("transform", `translate(${mar
                                                 .attr("y", 10)
                                                 .attr("fill", "currentColor")
                                                 .attr("text-anchor", "start"))
-                                                
+//deffining second  chart                                               
 
+const svg_deff = d3.select(element_2).append('svg').attr('height', height + (margin.r + margin.l)).attr('width', width + (margin.l + margin.r));
 
+const mainG_deff = svg_deff.append('g').attr('transform', `translate(${margin.l},${margin.t})`)
 
 
 
 mainG_off.append('g').attr('class', 'x-axis')
                      .attr('transform', `translate(0, ${height})`)
                      .call(d3.axisBottom(xScale))
-                     
+
+mainG_deff.append('g').attr('class', 'y-axis').attr("transform", `translate(0,0)`)
+                     .call(d3.axisLeft(yScaleDeff))
+                     .call(g => g.select(".domain").remove())
+                     .call(g => g.selectAll(".tick line").clone()
+                         .attr("x2", width  )
+                         .attr("stroke-opacity", 0.1))
+                     .call(g => g.append("text")
+                         .attr("x",  - margin.l)
+                         .attr("y", 10)
+                         .attr("fill", "currentColor")
+                         .attr("text-anchor", "start"))
+                         
 
 const max_line = d3.line()
                     .x(d=>xScale(d.year_date))
@@ -84,6 +107,34 @@ const mean_line = d3.line()
                     .y(d=>yScale(d.avg))
 
 
+  // add three point data line 
+  let data_three = agg_stats_off.filter(d=>['1998-99', '2013-14', '2023-24'].includes(d.year));
+  const data_for_threes ={
+      '1998-99':{total_three:'19k', text:'<- 19k total 3PA', offset:2},
+      '2013-14':{total_three:'52k', text:'<- 52k total 3PA', offset:2},
+      '2023-24':{total_three:'86k', text:'86k total 3PA ->', offset:-85}
+  }
+  data_three = data_three.map(d=>({...d, ...data_for_threes[d.year]}) )
+  const addThreePointsLines = (selection, data) =>{
+      selection.append('g').selectAll().data(data).join('line')
+                                      .attr('x1', d=>xScale(d.year_date)).attr('x2', d=>xScale(d.year_date))
+                                      .attr('y1',-10).attr('y2',height)
+                                      .attr('stroke',avg_line_color)
+                                      .attr('opacity', 0.65)
+                                      .attr('stroke-dasharray','5 5')
+
+      selection.append('g').selectAll().data(data).join('text')
+                                      .attr('x', d=>xScale(d.year_date))
+                                      .attr('y2',height)
+                                      .attr('fill', avg_line_color)
+                                      .attr('font-size', '10px')
+                                      .attr('transform', d=>`translate(${d.offset},-2)`)
+                                      .text(d=> d.text)
+  }
+
+  mainG_off.append('g').call(addThreePointsLines, data_three)
+  mainG_deff.append('g').call(addThreePointsLines, data_three)
+
 //visuals 
 console.log('agg_stats_off',agg_stats_off)
 mainG_off.append('g').append('path').attr('d', max_line(agg_stats_off)).attr('stroke', 'white').attr('stroke-width', 2.5).attr('fill', 'none')
@@ -95,31 +146,18 @@ mainG_off.append('g').append('path').attr('d', min_line(agg_stats_off)).attr('st
 mainG_off.append('g').append('path').attr('d', mean_line(agg_stats_off))
                                      .attr('stroke', 'white').attr('stroke-width', 1.5).attr('fill', 'none').attr('stroke-dasharray', '5 5')
 mainG_off.append('g').append('path').attr('d', mean_line(agg_stats_off))
-                                    .attr('stroke', 'lightgrey').attr('stroke-width', 1.5).attr('fill', 'none').attr('stroke-dasharray', '5 5')
+                                    .attr('stroke', avg_line_color).attr('stroke-width', 1.5).attr('fill', 'none').attr('stroke-dasharray', '5 5')
 
 //visuals 2 
 
-const svg_deff = d3.select(element_2).append('svg').attr('height', height + (margin.r + margin.l)).attr('width', width + (margin.l + margin.r));
 
-const mainG_deff = svg_deff.append('g').attr('transform', `translate(${margin.l},${margin.t})`)
 
-const yScaleDeff = d3.scaleLinear([d3.min(agg_stats_def, d=>d.min), d3.max(agg_stats_def, d=>d.max)], [0,height]).nice();
+
 
 //axis 2 
 
 //axis 1
-mainG_deff.append('g').attr('class', 'y-axis').attr("transform", `translate(${margin.l}},0)`)
-                                            .call(d3.axisLeft(yScaleDeff))
-                                            .call(g => g.select(".domain").remove())
-                                            .call(g => g.selectAll(".tick line").clone()
-                                                .attr("x2", width  )
-                                                .attr("stroke-opacity", 0.1))
-                                            .call(g => g.append("text")
-                                                .attr("x",  - margin.l)
-                                                .attr("y", 10)
-                                                .attr("fill", "currentColor")
-                                                .attr("text-anchor", "start"))
-                                                
+
 
 
 
@@ -149,13 +187,13 @@ mainG_deff.append('g').append('path').attr('d', min_line_deff(agg_stats_def)).at
 mainG_deff.append('g').append('path').attr('d', mean_line_deff(agg_stats_def))
                                                          .attr('stroke', 'white').attr('stroke-width', 1.5).attr('fill', 'none').attr('stroke-dasharray', '5 5')
 mainG_deff.append('g').append('path').attr('d', mean_line_deff(agg_stats_def))
-                                                        .attr('stroke', 'lightgrey').attr('stroke-width', 1.5).attr('fill', 'none').attr('stroke-dasharray', '5 5')
+                                                        .attr('stroke', avg_line_color).attr('stroke-width', 1.5).attr('fill', 'none').attr('stroke-dasharray', '5 5')
 
 //Leged_1 
 //legend type {text:, dashArray:, fill:}
 
 const legend_off_data =[{text:"Best Team Offense", dashArray:"0", fill:'#1D428A' }, 
-                        {text:"Avg Team Offense",dashArray:"5 5", fill:'lightgrey'}, 
+                        {text:"Avg Team Offense",dashArray:"5 5", fill:'grey'}, 
                         {text:"Worst Team Offense", dashArray:"0", fill:'#C8102E'}];
 
 function legend(selection, data){
@@ -174,6 +212,115 @@ const legend_def_data =[{text:"Best Team Defense", dashArray:"0", fill:'#1D428A'
 
 mainG_deff.append('g').attr('class','legend').attr('transform', `translate(${-18},${height + 40})`).call(legend,legend_def_data);
 
+//anotations
+
+const data_2022 = agg_stats_off.filter(d=>d.year==='2022-23')
+const anot_22_type = annotationLabel;
+const annoOff22 = [
+    {
+      note: {
+        label: "",
+        title: "The Hornets posted an Off Rating of 108.4",
+        wrap:200
+      },
+      data:data_2022[0],
+      connector: {
+        end: "arrow",        // Can be none, or arrow or dot
+        type: "line",      // ?? don't know what it does
+        lineType : "vertical",    // ?? don't know what it does
+        //endScale: 2     // dot size
+      },
+      color: ["grey"],
+      dy: 125,
+      dx: -10
+
+    }
+ ]
+
+ const makeOff22Anno = annotation().notePadding(0).type(anot_22_type).accessors({
+    x: d => xScale(d.year_date)+0.5,
+    y: d => yScale(d.min)+3,
+  }).annotations(annoOff22);
+
+  mainG_off.append('g').call(makeOff22Anno)
+
+  //annoitation two
+  const data_2018 = agg_stats_off.filter(d=>d.year==='2018-19')
+  const anot_2_type  =  annotationLabel;
+  const anno_2 = [
+    {
+      note: {
+        label: "this off rating the worst rating of the 2022 season. This rating is 1.8 points higher than the league leading 1998 Pacers",
+        title: "Avg, Best and Worst Offenses has all gone up.",
+        wrap:200
+      },
+      data:data_2018[0],
+      connector: {
+        end: "arrow",        // Can be none, or arrow or dot
+        type: "line",      // ?? don't know what it does
+        lineType : "vertical",    // ?? don't know what it does
+        //endScale: 2     // dot size
+      },
+      color: ["grey"],
+      dy: 325,
+      dx: -10
+
+    }
+ ]
+
+ const makeAnon_2 = annotation().notePadding(0).type(anot_2_type).accessors({
+  x: d => xScale(d.year_date),
+  y: d => yScale(d.avg) +45,
+}).annotations(anno_2);
+
+mainG_off.append('g').call(makeAnon_2)
+
+
+//annoitation three
+const data_2018_deff = agg_stats_def.filter(d=>d.year==='2018-19')
+const anot_3_type  =  annotationLabel;
+const anno_3 = [
+  {
+    note: {
+      label: "Best Wrose and Avg defense have all gone down with teams only finding solutions relative to eachother.",
+      title:"Defense has gone down in Tandem" ,
+      wrap:175
+    },
+    data:data_2018_deff[0],
+    connector: {
+      end: "arrow",        // Can be none, or arrow or dot
+      type: "line",      // ?? don't know what it does
+      lineType : "vertical",    // ?? don't know what it does
+      //endScale: 2     // dot size
+    },
+    color: ["grey"],
+    dy: -135,
+    dx: 10
+
+  }
+]
+
+const makeAnon_3 = annotation().notePadding(0).type(anot_3_type).accessors({
+x: d => xScale(d.year_date),
+y: d => yScaleDeff(d.avg) - 20,
+}).annotations(anno_3);
+
+mainG_deff.append('g').call(makeAnon_3)
+
+
+  //append text 
+    mainG_off.append('g').selectAll().data(data_2022).join('text')
+                                    .attr('x', d=>xScale(d.year_date)-14).attr('y', d=>yScale(d.min)-2).attr('fill', '#C8102E')
+                                    .attr('font-size', '12px')
+                                    .text(d=>d.min)
+
+    mainG_off.append('g').selectAll().data(agg_stats_off.filter(d=>d.year==='1998-99')).join('text')
+                                    .attr('x', d=>xScale(d.year_date)-14).attr('y', d=>yScale(d.max)+10).attr('fill', '#1D428A')
+                                    .attr('font-size', '12px')
+                                    .text(d=>d.max)
+    
+    
+  
 }
 
 
--- nba_over_time/src/main.js
+++ nba_over_time/src/main.js
@@ -4,26 +4,38 @@ import {createChart} from './chart';
 
 
 document.querySelector('#app').innerHTML = `
-  <div>
-  <div>
+  <div id="main-wrapper">
+  <div id="sub-main-cont">
   <div id="title">
     <div id="mainTitle">
-      Today's NBA Worst Offenses Are Better Than Yesterdays Best NBA Offenses.
+      In Today's NBA The Worst Offense is Better Than Yesterday's NBA's Best NBA Offenses.
     </div>
     <div id="subTitle">
-      The 2022-23 season the team with the worst NBA Offense was better than the best offense from the 1998-99 season. Every single team has improved their offense with the 2023-20224 season ending with and average offense over 10+ PPP100 than the 2011-12 Season.
-    </div>
+      Both Defense and Offense in the NBA has moved. The average NBA offense is 7.47 points higher than the best NBA offense in 1998. The Average NBA Defense is 5.91 poits worse than the best defense in 1998.
+      <p>Both, NBA Teams offense and defense move in tandem. As NBA teams have copied eachothers offenses, in couting specific players, developing a modern offense and players have come to the league prepared to play modern offenses,  the worst NBA offenses today are better than the best of yesterday.
+      As a result, NBA offense and defense can only be effective as comparable to the average NBA defense , in it's year.  </p>
     <div id="authorTitle">
       By: <a id="me" href="https://bsky.app/profile/truesync.bsky.social">Nathan Harris</a>     <a href="https://bsky.app/profile/truesync.bsky.social">@truesync.bsky.social</a> <a href="http://nathanharris.co">nathanharris.co</a> 
     </div>
 
   </div>
-    <div id="chart-1-title"> Offensive Ratings</div>
-    <div id="chart_1"></div>
+    <div id="chart-1-title" class="a-chart-title"> Offensive Ratings</div>
+    <div id="chart_1" ></div>
   </div>
   <div>
-  <div id="chart-2-title"> Deffensive Ratings</div>
-    <div id="chart_2"></div>
+  <div class="mid-para">
+    The underlying drivers of this explosion in offense is not something that one team has been able to monopolize and has spread to all 30 teams. For example when the Warriors destroyed the record for threes in a season
+    we saw other teams ie the Celtics blow past that record and now every team shoots more threes. Both external factors and internal factors have been quickly adopted and propagated across the league. 
+
+    <p>
+     Looking at Defensive effeciency numbers highlights how no team has been able to develope a defensive strategy to nullify the underlying drivers of Modern NBA offensive explosion. Every year collectively NBA Defenses now have trended down.
+     With the Average NBA Defense 5.91 points worse than the worst NBA Defense in 1998.
+    </p>
+  </div>
+  </div>
+  <div>
+  <div id="chart-2-title" class="a-chart-title"> Deffensive Ratings</div>
+    <div id="chart_2" ></div>
   </div>
   </div>
 `
